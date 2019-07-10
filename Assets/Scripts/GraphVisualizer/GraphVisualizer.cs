6e2f3bbf8b00cfc60f960ea17e4ab75742872774
Denis Yelchaninov <denisjelczaninov@gmail.com>
2019-07-10 16:04:28 +0200

    Added drawing lines between graphnodes

--- Assets/Scripts/GraphVisualizer/GraphVisualizer.cs
+++ Assets/Scripts/GraphVisualizer/GraphVisualizer.cs
@@ -8,17 +8,22 @@ public class GraphVisualizer : MonoBehaviour
     public GameObject elNode;
     public static float nodeRadius = 9;
     public static float levelHeight = 36;
+
     List<VisualNode> graphNodes = new List<VisualNode>();
     List<INode> nodes = new List<INode>();
     Dictionary<INode, VisualNode> nodeToVisual = new Dictionary<INode, VisualNode>();
     List<List<VisualNode>> nodesInDepth = new List<List<VisualNode>>();
+    List<GameObject> lines;
     INode root;
     float scale = 1;
     int maxDepth;
 
-    public void DrawGraph()
+    //TODO make hierarchy object for assigning parents to the "lines" object and "nodes" object on scene
+    public void DrawGraph(INode root)
     {
-
+        ExtractGraph(root);
+        Reposition();
+        DrawConnections();
     }
 
     public void Scale(bool sc)
@@ -33,12 +38,14 @@ public class GraphVisualizer : MonoBehaviour
 
     public void ExtractGraph(INode root)
     {
+        this.root = root;
         List<INode> toCheck = new List<INode>();
         List<INode> newToCheck = new List<INode>();
         int depth = 0;
         toCheck.Add(root);
 
         VisualNode rootV = Instantiate(elNode).GetComponent<VisualNode>();
+        rootV.transform.parent = transform;
 
         rootV.Initialize(root, null, depth);
         nodeToVisual.Add(root, rootV);
@@ -99,7 +106,35 @@ public class GraphVisualizer : MonoBehaviour
 
     private void DrawConnections()
     {
+        lines = new List<GameObject>();
+        List<VisualNode> toDraw = new List<VisualNode>();
+        List<VisualNode> newToDraw = new List<VisualNode>();
 
+        toDraw.Add(nodeToVisual[this.root]);
+
+        while (toDraw.Count != 0)
+        {
+            foreach (VisualNode node in toDraw)
+            {
+                foreach (VisualNode childNode in node.childrenVisual)
+                {
+                    GameObject line = new GameObject();
+                    lines.Add(line);
+                    var lr = line.AddComponent<LineRenderer>();
+                    lr.startWidth = 0.05F;
+                    lr.endWidth = 0.05F;
+                    lr.positionCount = 2;
+                    Vector3[] positions = new Vector3[2];
+                    positions[0] = node.transform.position;
+                    positions[1] = childNode.transform.position;
+                    lr.SetPositions(positions);
+
+                    newToDraw.Add(childNode);
+                }
+            }
+            toDraw = new List<VisualNode>(newToDraw);
+            newToDraw = new List<VisualNode>();
+        }
     }
 
     private int CountWidth(List<VisualNode> nodes)
